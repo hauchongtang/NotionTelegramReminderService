6ca3ca633aaa2faf7bb7d0346f5ccfe350ffbcb8
Hau Chong <56034480+hauchongtang@users.noreply.github.com>
2025-05-28 23:05:04 +0800

    Update TransportConfiguration.cs

--- NotionReminderService/Config/TransportConfiguration.cs
+++ NotionReminderService/Config/TransportConfiguration.cs
@@ -4,4 +4,5 @@ public class TransportConfiguration
 {
     public required string DataMallUrl { get; set; }
     public required string AccountKey { get; set; }
+    public required double Radius { get; set; }
 }
